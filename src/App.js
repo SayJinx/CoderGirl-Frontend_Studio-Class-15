2dc92eb9a0e7a7e22aae38ec80319a10d846734c
SayJinx <whatsupguest@gmail.com>
2021-10-13 10:19:17 -0500

    finished linking.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { NavLink, Route } from 'react-router-dom'
 import Blog from './Blog'
+import Contact from './Contact'
+import Welcome from './Welcome'
+import Message from './Message'
 // TO DO: Add the Proper Routes to the components Listed Below!
 
 
@@ -9,12 +12,40 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-      <Switch>
+
+      <header>
+
+        <Route className='App' exact path='/home'>
+          <Welcome />
+        </Route>
+        <Route exact path='/about'>
+          <Message />
+        </Route>
         <Route exact path='/blog'>
           <Blog />
         </Route>
-      </Switch>
+        <Route exact path='/contact'>
+          <Contact />
+        </Route>
 
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/home'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/blog'>Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to='/about'>About</NavLink>
+            </li>
+            <li>
+              <NavLink to='/contact'>Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+      </header>
       </div>
   );
 }
